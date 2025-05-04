ff06be36be66826cc4ba88633d5beaaf5d158ed6
James Onashile <jonashile@gmail.com>
2025-05-04 15:19:53 +0100

    feat: add dummy country data

--- components/WorldMap.tsx
+++ components/WorldMap.tsx
@@ -1,11 +1,11 @@
 export default function WorldMap(){
     return (
-        <svg
-            width="100%"
-            height="100%"
-            viewBox="0 0 800 400"
-            xmlns="http://www.w3.org/2000/svg"
-            className="bg-gray-100"
-        ><rect x="0" y="0" width="800" height="400" fill="blue" /></svg>
-    )
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 800 400"
+        xmlns="http://www.w3.org/2000/svg"
+        className="bg-gray-100"
+      ></svg>
+    );
 }
---
+++ lib/countries-data.ts
@@ -0,0 +1,37 @@
+export const countries = [
+    {
+        name: "United States",
+        code: "US",
+        dividendPhase: "Contracting Pillar",
+        demographicShape: "Barrel",
+        policyScore: 7.5
+    },
+    {
+        name: "China",
+        code: "CN",
+        dividendPhase: "Post-Dividend",
+        demographicShape: "Inverted Pyramind",
+        policyScore: 6
+    },
+    {
+        name: "Germany",
+        code: "DE",
+        dividendPhase: "Post-Dividend",
+        demographicShape: "Contracting Pillar",
+        policyScore: 4
+    },
+    {
+        name: "Japan",
+        code: "JP",
+        dividendPhase: "Post-Dividend",
+        demographicShape: "Contracting Pillar",
+        policyScore: 4
+    },
+    {
+        name: "India",
+        code: "IN",
+        dividendPhase: "Entering Dividend",
+        demographicShape: "Stationary Column",
+        policyScore: 4
+    }
+]
