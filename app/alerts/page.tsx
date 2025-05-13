7b70d83742e869c715aa45db300e10baf59d3cbf
James Onashile <jonashile@gmail.com>
2025-05-13 21:43:43 +0100

    feat: mobile responsive tweaks and scrollable modals

--- app/alerts/page.tsx
+++ app/alerts/page.tsx
@@ -2,10 +2,11 @@ import AlertList from "@/components/AlertList";
 
 export default function AlertsPage() {
     return(
-      <main className="p-6 max-w-3xl mx-auto">
+      <main className="p-4 sm:p-6 max-w-3xl mx-auto">
         <h1 className="text-2xl font-bold mb-4">Structural Alerts</h1>
         <AlertList/>
       </main>
     );
   }
+  
   
--- components/AlertCard.tsx
+++ components/AlertCard.tsx
@@ -6,7 +6,7 @@ type AlertCardProps = {
 
 export default function AlertCard({ alert }: AlertCardProps ) {
   return (
-    <div className="border p-3 rounded-md bg-white shadow-sm">
+    <div className="border p-4 rounded-md bg-white shadow-sm space-y-1 text-sm sm:text-base">
       <div className="text-sm text-gray-600">{new Intl.DateTimeFormat("en-US", { dateStyle: "medium"}).format(new Date(alert.triggeredAt))}</div>
       <div className="font-semibold text-gray-800">{alert.signalType}</div>
       <div className="texct-xs text-gray-500">
